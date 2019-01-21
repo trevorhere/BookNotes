75d87a735f86e3a088f9505dff01be5fe6f5e8c3
trevorhere <trevoranthonylane@gmail.com>
2019-01-21 00:12:52 -0700

    test

--- react-ui/src/components/Nav.js
+++ react-ui/src/components/Nav.js
@@ -4,7 +4,7 @@ import { Navbar, NavItem, Preloader } from "react-materialize";
 class Nav extends Component {
   render() {
     return (
-      <Navbar brand="logo" right>
+      <Navbar brand="retron" right>
         <NavItem onClick={() => console.log("test click")}>
           Getting started
         </NavItem>
